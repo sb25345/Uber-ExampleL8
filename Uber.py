f3c00d1042decfca624cd4db9b64227c7354e796
sb25345 <sba25345@student.cct.ie>
2026-05-23 15:06:27 +0100

    Update Uber.py

--- Uber.py
+++ Uber.py
@@ -5,7 +5,7 @@ import numpy as np
 
 st.title("Uber Pickups in New York")
 
-DATE_COLUMN = ('date/time'
+DATE_COLUMN = 'date/time'
 DATE_URL = ('HTTPS://S3-US-WEST-2.AMAZONAWS.COM/)'
         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
